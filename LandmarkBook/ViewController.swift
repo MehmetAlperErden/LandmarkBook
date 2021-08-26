59fc039ea882b5ab8eb7c8892e68d36fcd40acd5
example <email@example.com>
2021-08-26 17:14:44 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Alper Erden on 26.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
